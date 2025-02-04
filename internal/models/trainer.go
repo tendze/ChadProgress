23e9f18cd718748730ee30e15e30f72d478c90ba
Максим <dang.maxim2811@gmail.com>
2025-02-04 07:25:44 +0300

    trainer status added

--- internal/models/trainer.go
+++ internal/models/trainer.go
@@ -6,6 +6,7 @@ type Trainer struct {
 	Qualifications  string           `gorm:"type:varchar(150)"`
 	Experience      string           `gorm:"type:varchar(250)"`
 	Achievements    string           `gorm:"type:varchar(250)"`
+	Status          string           `gorm:"type:ENUM('ACTIVE', 'BUSY', 'ON_VACATION');default:'ACTIVE';not null"`
 	Clients         []Client         `gorm:"foreignKey:TrainerID"`
 	TrainingPlans   []TrainingPlan   `gorm:"foreignKey:TrainerID"`
 	ProgressReports []ProgressReport `gorm:"foreignKey:TrainerID"`
